4b08af0e7a5c42c15127b8dc41e451cf970e541e
Avior Kahalani <59415129+AviorKahalani@users.noreply.github.com>
2022-07-05 20:58:55 +0300

    feat: add icons and fix issue with unexisting user pics

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Dropdown } from './Dropdown'
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa'
+import { SiAuth0 } from 'react-icons/si'
 
 export const AppHeader = ({ user, onLogout }) => {
   const [isShown, setIsShown] = useState(false)
@@ -10,11 +12,23 @@ export const AppHeader = ({ user, onLogout }) => {
 
   return (
     <header className="main-header flex justify-between items-center">
-      <div className="logo">AuthApp</div>
+      <div className="logo flex gap-1 items-center">
+        <SiAuth0 />
+        AuthApp
+      </div>
       <section onClick={() => handleClick()}>
         <div className="user-info flex gap-1 items-center">
-          <img className="user__photo" src={user.photoURL} alt="" />
+          <img
+            className="user__photo"
+            src={
+              user.photoURL
+                ? user.photoURL
+                : new URL('../assets/user.png', import.meta.url).href
+            }
+            alt=""
+          />
           <p>{user.displayName}</p>
+          {isShown ? <FaAngleUp /> : <FaAngleDown />}
         </div>
         {isShown && <Dropdown onLogout={onLogout} />}
       </section>
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaSignOutAlt, FaRegUser } from 'react-icons/fa'
 
 export const Dropdown = ({ onLogout }) => {
   return (
     <div className="dropdown flex flex-col ">
-      <div className="__profile">
-        <Link to="profile">My Profile</Link>
+      <div className="__profile flex items-center gap-1">
+        <FaRegUser />
+        <Link to="profile">
+          <span>My Profile</span>
+        </Link>
       </div>
       <div className="__divider"></div>
-      <div className="__logout" onClick={() => onLogout()}>
-        Logout
+      <div className="__logout flex items-center gap-1" onClick={() => onLogout()}>
+        <FaSignOutAlt />
+        <span>Logout</span>
       </div>
     </div>
   )
